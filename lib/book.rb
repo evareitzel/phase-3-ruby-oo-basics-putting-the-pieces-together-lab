41627a5d8e6eb3581f9ab780300ec2dfa04b2a8a
Eva Reitzel <evareitzel@gmail.com>
2022-11-18 14:21:50 -0500

    commit tree 4c7149f6d5969eccdad2239c37d19d216bb2dfd9

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,15 @@
+class Book
+  attr_accessor :title, :author, :page_count, :genre, :turn_page
+  
+  def initialize(title, author="Agatha Christie", page_count=272, genre="Mystery")
+    @title = title
+    @author = author
+    @page_count = page_count
+    @genre = genre
+  end
 
+  def turn_page
+    @turn_page = puts "Flipping the page...wow, you read fast!"
+  end
 
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,19 @@
 # Make your shoe class here!
+
+class Shoe
+  attr_accessor :brand, :color, :size, :material, :condition, :cobble
+
+  def initialize(brand, color="red", size=9.5, material="suede", condition="tattered") # cobble(new/has been repaired)
+    @brand = brand
+    @color = color
+    @size = size
+    @material = material
+    @condition = condition
+  end
+
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+  end
+
+end
